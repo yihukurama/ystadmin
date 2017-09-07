3959645b84b7ec9315acc7b1b6ca6a6882ca3ba3
Administrator <Administrator@YUNST-03>
2017-09-07 17:49:16 +0800

    后台管理系统菜单

--- ystadmin/src/main/java/com/gdyunst/ystadmin/framework/service/domainservice/admin/impl/ViewService.java
+++ ystadmin/src/main/java/com/gdyunst/ystadmin/framework/service/domainservice/admin/impl/ViewService.java
@@ -2,17 +2,28 @@ package com.gdyunst.ystadmin.framework.service.domainservice.admin.impl;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import com.alibaba.fastjson.JSON;
+import com.gdyunst.ystadmin.application.config.SystemConfig;
 import com.gdyunst.ystadmin.application.exception.CrudException;
+import com.gdyunst.ystadmin.framework.domain.entity.admin.FuncEntity;
 import com.gdyunst.ystadmin.framework.domain.entity.admin.MenuEntity;
 import com.gdyunst.ystadmin.framework.domain.entity.admin.UserEntity;
+import com.gdyunst.ystadmin.framework.service.domain.admin.Menu;
 import com.gdyunst.ystadmin.framework.service.domain.admin.User;
 import com.gdyunst.ystadmin.framework.service.domainservice.admin.IViewService;
+import com.gdyunst.ystadmin.framework.service.domainservice.prepare.base.LogUtil;
 @Service
 public class ViewService implements IViewService {
 
+	@Autowired
+	SystemConfig systemConfig;
     @Override
     public Model initAdminIndex(Model model,String userId) {
        
@@ -25,8 +36,23 @@ public class ViewService implements IViewService {
                 return null;
             }
             user.doGetRelationData();
+            List<Menu> userMenus = new ArrayList<Menu>();
+            List<Menu> userMenu = user.getMenuPrivileges();
+            for (Menu menu : userMenu) {
+				if(menu.getSubSystemId().equals(systemConfig.getAdminSystemId())) {
+					userMenus.add(menu);
+				}
+			}
+            List<FuncEntity> userFuncs = new ArrayList<FuncEntity>();
+            List<FuncEntity> userFunc = user.getFuncPrivileges();
+            for (FuncEntity funcEntity : userFunc) {
+				if(funcEntity.getSubSystemId().equals(systemConfig.getAdminSystemId())) {
+					userFuncs.add(funcEntity);
+				}
+			}
+            
             model.addAttribute("userName", user.getUsername());
-            model.addAttribute("menus", user.getMenuPrivileges());
+            model.addAttribute("menus", userMenus);
         } catch (CrudException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
